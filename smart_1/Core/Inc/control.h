69b90c63777c4e88d900643f3ad4545660413458
laptoy <2023320969@qq.com>
2024-08-28 14:23:04 +0800

    smart_1

---
+++ smart_1/Core/Inc/control.h
@@ -0,0 +1,33 @@
+#ifndef __CONTROL_H
+#define __CONTROL_H
+
+#include <tim.h>
+#include <stdio.h>
+#include <main.h>
+#include <pid.h>
+#include <gpio.h>
+
+void Motor_Init(void);
+
+void Motor1_SetSpeed(uint8_t Dir, uint16_t Speed);
+
+void Motor2_SetSpeed(uint8_t Dir, uint16_t Speed);
+
+void Motor3_SetSpeed(uint8_t Dir, uint16_t Speed);
+
+void Motor4_SetSpeed(uint8_t Dir, uint16_t Speed);
+
+void Car_stop();
+
+void Car_forward(uint16_t Speed);
+
+void Car_backward(uint16_t Speed);
+
+void Car_left(uint16_t Speed);
+
+void Car_right(uint16_t Speed);
+
+
+
+
+#endif
